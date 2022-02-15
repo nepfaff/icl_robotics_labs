07e08bdd7e4a7b237edc7bab948d52d20c452af9
nicholas-p1 <nicholas.pfaff19@imperial.ac.uk>
2022-02-15 17:05:47 +0000

    Add lab 5 files

---
+++ lab5/mcl.lua
@@ -0,0 +1,161 @@
+-- Generate a sample from a Gaussian distribution
+function gaussian (mean, variance)
+    return  math.sqrt(-2 * variance * math.log(math.random() + 0.00001)) *
+            math.cos(2 * math.pi * math.random()) + mean
+end
+
+function createRandomBumpyFloor()
+    print ("Generating new random bumpy floor.")
+    sim.setThreadAutomaticSwitch(false)
+
+    -- Remove existing bumpy floor if there already is one
+    if (heightField ~= nil) then
+        sim.setObjectPosition(heightField, heightField, {0.05, 0, 0})
+        return
+    end
+    --  Create random bumpy floor for robot to drive on
+    floorSize = 5
+    --heightFieldResolution = 0.3
+    --heightFieldNoise = 0.00000005
+    heightFieldResolution = 0.1
+    heightFieldNoise = 0.0000008
+    cellsPerSide = floorSize / heightFieldResolution
+    cellHeights = {}
+    for i=1,cellsPerSide*cellsPerSide,1 do
+        table.insert(cellHeights, gaussian(0, heightFieldNoise))
+    end
+    heightField=sim.createHeightfieldShape(0, 0, cellsPerSide, cellsPerSide, floorSize, cellHeights)
+    -- Make the floor invisible
+    sim.setObjectInt32Param(heightField,10,0)
+    sim.setThreadAutomaticSwitch(true)
+end
+
+
+function get_walls()
+    -- Disable error reporting
+    local savedState=sim.getInt32Param(sim.intparam_error_report_mode)
+    sim.setInt32Param(sim.intparam_error_report_mode,0)
+    local N = 1
+    while true do
+        local handle = sim.getObjectHandle("Wall"..tostring(N))
+        if handle <= 0 then
+            break
+        end
+
+        -- Read position and shape of wall
+        -- Assume here that it is thin and oriented either along the x axis or y axis
+
+        -- We can now get the propertries of these walls, e.g....
+        local pos = sim.getObjectPosition(handle, -1)
+        local res,minx = sim.getObjectFloatParameter(handle,15)
+        local res,maxx = sim.getObjectFloatParameter(handle,18)
+        local res,miny = sim.getObjectFloatParameter(handle,16)
+        local res,maxy = sim.getObjectFloatParameter(handle,19)
+
+        --print("Position of Wall " .. tostring(N) .. ": " .. tostring(pos[1]) .. "," .. tostring(pos[2]) .. "," .. tostring(pos[3]))
+        --print("minmax", minx, maxx, miny, maxy)
+
+        local Ax, Ay, Bx, By
+        if (maxx - minx > maxy - miny) then
+            print("Wall " ..tostring(N).. " along x axis")
+            Ax = pos[1] + minx
+            Ay = pos[2]
+            Bx = pos[1] + maxx
+            By = pos[2]
+        else
+            print("Wall " ..tostring(N).. " along y axis")
+            Ax = pos[1]
+            Ay = pos[2] + miny
+            Bx = pos[1]
+            By = pos[2] + maxy
+        end
+        print (Ax, Ay, Bx, By)
+
+        walls[N] = {Ax, Ay, Bx, By}
+        N = N + 1
+    end
+    -- enable error reporting
+    sim.setInt32Param(sim.intparam_error_report_mode,savedState)
+
+    return N - 1
+end
+
+
+-- This function is executed exactly once when the scene is initialised
+function sysCall_init()
+
+    tt = sim.getSimulationTime()
+    print("Init hello", tt)
+
+    robotBase=sim.getObjectHandle(sim.handle_self) -- robot handle
+    leftMotor=sim.getObjectHandle("leftMotor") -- Handle of the left motor
+    rightMotor=sim.getObjectHandle("rightMotor") -- Handle of the right motor
+    turretMotor=sim.getObjectHandle("turretMotor") -- Handle of the turret motor
+    turretSensor=sim.getObjectHandle("turretSensor")     
+
+    -- Create bumpy floor for robot to drive on
+    createRandomBumpyFloor()
+
+    N_WAYPOINTS = 26
+    currentWaypoint = 0
+    waypoints = {}
+    waypoints[1] = {0.5,0}
+    waypoints[2] = {1,0}
+    waypoints[3] = {1,0.5}
+    waypoints[4] = {1,1}
+    waypoints[5] = {1,1.5}
+    waypoints[6] = {1,2}
+    waypoints[7] = {0.5,2}
+    waypoints[8] = {0,2}
+    waypoints[9] = {-0.5,2}
+    waypoints[10] = {-1,2}
+    waypoints[11] = {-1,1.5}
+    waypoints[12] = {-1,1}
+    waypoints[13] = {-1.5,1}
+    waypoints[14] = {-2,1}
+    waypoints[15] = {-2,0.5}
+    waypoints[16] = {-2,0}
+    waypoints[17] = {-2,-0.5}
+    waypoints[18] = {-1.5,-1}
+    waypoints[19] = {-1,-1.5}
+    waypoints[20] = {-0.5,-1.5}
+    waypoints[21] = {0,-1.5}
+    waypoints[22] = {0.5,-1.5}
+    waypoints[23] = {1,-1.5} 
+    waypoints[24] = {1,-1}
+    waypoints[25] = {0.5,-0.5}
+    waypoints[26] = {0,0}
+
+    -- Data structure for walls
+    walls = {}
+    -- Fill it by parsing the scene in the GUI
+    N_WALLS = get_walls()
+    -- walls now is an array of arrays with the {Ax, Ay, Bx, By} wall coordinates
+
+    sensorStandardDeviation = 0.1
+    sensorVariance = sensorStandardDeviation^2
+    noisyDistance = 0
+end
+
+function sysCall_sensing()
+    
+end
+
+
+
+
+function sysCall_actuation() 
+    tt = sim.getSimulationTime()
+    -- print("actuation hello", tt)
+
+    result,cleanDistance=sim.readProximitySensor(turretSensor)
+
+    if (result>0) then
+        noisyDistance= cleanDistance + gaussian(0.0, sensorVariance)
+        --print ("Depth sensor reading ", noisyDistance)
+    end 
+end
+
+function sysCall_cleanup()
+    --simUI.destroy(ui)
+end 
