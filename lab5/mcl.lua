9fcb3570d5436233c585334e9698c636c5cbc3d9
nicholas-p1 <nicholas.pfaff19@imperial.ac.uk>
2022-02-21 16:01:08 +0000

    Increase motion update noise variances

--- lab5/mcl.lua
+++ lab5/mcl.lua
@@ -197,11 +197,11 @@ function sysCall_init()
 
     -- Zero mean Gaussian noise variance in meter/radians (to calibrate)
     -- Determined for a one meter distance
-    straightLineXYVariance = 0.0005
-    straightLineThetaVariance = 0.0005
+    straightLineXYVariance = 0.0010
+    straightLineThetaVariance = 0.0010
     -- Zero mean Gaussian noise variance in radians (to calibrate)
     -- Determined for a one radian rotation
-    rotationThetaVariance = 0.003
+    rotationThetaVariance = 0.004
 end
 
 function sysCall_sensing()
