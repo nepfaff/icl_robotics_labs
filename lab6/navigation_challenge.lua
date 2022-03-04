92b9bb343b39a72373a29b13e2083f86e2889f33
nicholas-p1 <nicholas.pfaff19@imperial.ac.uk>
2022-03-04 14:40:30 +0000

    Change point estimation from mean to median

--- lab6/navigation_challenge.lua
+++ lab6/navigation_challenge.lua
@@ -688,17 +688,40 @@ function isWaypointGoalWaypoint(waypoint)
     return false
 end
 
+function median(t)
+    local temp={}
+  
+    -- deep copy table so that when we sort it, the original is unchanged
+    for _,v in pairs(t) do
+        table.insert( temp, v )
+    end
+  
+    table.sort(temp)
+  
+    -- If we have an even number of table elements or odd.
+    if math.fmod(#temp,2) == 0 then
+      -- return mean value of middle two elements
+      return ( temp[#temp/2] + temp[(#temp/2)+1] ) / 2
+    else
+      -- return middle element
+      return temp[math.ceil(#temp/2)]
+    end
+  end
+
 
 function pointEstimateX()
-    return weighted_sum(xArray, weightArray)
+    --return weighted_sum(xArray, weightArray)
+    return median(xArray)
 end
 
 function pointEstimateY()
-    return weighted_sum(yArray, weightArray)
+    -- return weighted_sum(yArray, weightArray)
+    return median(yArray)
 end
 
 function pointEstimateTheta()
-    return weighted_sum(thetaArray, weightArray)
+    -- return weighted_sum(thetaArray, weightArray)
+    return median(thetaArray)
 end
 
 
